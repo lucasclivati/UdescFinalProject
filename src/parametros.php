5966f2212c5640ca8bf7c3a59f592a5d7f421e4a
lucasclivati <lucascli@hotmail.com>
2022-12-31 16:19:39 -0300

    Update parametros.php

--- src/parametros.php
+++ src/parametros.php
@@ -1,6 +1,13 @@
 <?php
-$dadosPdo = [
+$dadosPDO = [
     'dns' => 'mysql:host=sql206.byetcluster.com;dbname=epiz_33214230_db1;charset=utf8',
     'usuario' => 'epiz_33214230',
     'senha' => '87c6vq1x'
-];?>
+];
+
+
+$db = new PDO($dadosPDO['dns'], $dadosPDO['usuario'], $dadosPDO['senha']);
+
+
+?>
+
