4c120e8ce6698524742d6b1a93d06bf3e034e6a3
Lucas Antonio Clivati <67926182+lucasclivati@users.noreply.github.com>
2023-01-14 13:06:20 -0300

    Update listarnoticias.php

--- src/listarnoticias.php
+++ src/listarnoticias.php
@@ -25,30 +25,30 @@ if(!$noticias) {
     throw new Exception('Não foi possível carregar as notícias');
 }
 
-
-
+$html = '';
+foreach ($noticias as $noticia) {
 $html = '
             <div class="col-md-6">
                 <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                     <div class="col p-4 d-flex flex-column position-static">
-                        <h3 class="mb-0">'. $noticias[0]['nottitulo'] . '</h3>
-                        <div class="mb-1 text-muted">'. $noticias[0]['notdatacriado'] . '</div>
-                        <p class="card-text mb-auto">'. $noticias[0]['notsubtitulo'] . '</p>
-                        <a href="http://lucascliprojetofinaludesc.infinityfreeapp.com/noticia.php?idnoticia='. $noticias[0]['idnoticia'] . '" class="stretched-link">Ver notícia.</a>
+                        <h3 class="mb-0">'. $noticia['nottitulo'] . '</h3>
+                        <div class="mb-1 text-muted">'. $noticia['notdatacriado'] . '</div>
+                        <p class="card-text mb-auto">'. $noticia['notsubtitulo'] . '</p>
+                        <a href="http://lucascliprojetofinaludesc.infinityfreeapp.com/noticia.php?idnoticia='. $noticia['idnoticia'] . '" class="stretched-link">Ver notícia.</a>
                     </div>
                     <div class="col-auto d-none d-lg-block">
-                    <img class="bd-placeholder-img" width="200" height="250" src="/files/notimgs/'.$noticias[0]['notimagem'].'" alt="Imagem">
+                    <img class="bd-placeholder-img" width="200" height="250" src="/files/notimgs/'.$noticia['notimagem'].'" alt="Imagem">
                     </div>
                 </div>
             </div>
             ';
-
+    }
 }  catch (Exception $e) {
     $html = '<p style="m-2"> Não foi possível carregar as notícias.</p>';
 }
 
 echo $html;
 
-
+var_dump($noticiaseach);
 
 ?>
